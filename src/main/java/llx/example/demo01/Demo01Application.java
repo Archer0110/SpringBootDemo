a4679a8ad9eec24792aeb36c641cbd7687206618
lilingxia <ctzk120@ctzk.com>
2018-07-17 17:50:20 +0800

    提交遗漏代码

--- src/main/java/llx/example/demo01/Demo01Application.java
+++ src/main/java/llx/example/demo01/Demo01Application.java
@@ -1,9 +1,11 @@
 package llx.example.demo01;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class Demo01Application {
 
     public static void main(String[] args) {
